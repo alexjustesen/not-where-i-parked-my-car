922192fdd0ef5776e05bfe783fac6a21d30e6e23
Alexander Justesen <alex.justesen@gmail.com>
2020-11-14 22:12:05 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+        defaultLineHeights: true,
+        standardFontWeights: true,
+    },
+    purge: [
+        './public/**/*.html'
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
